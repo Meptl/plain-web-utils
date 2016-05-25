070c15da901e7c05b2c5d619d7158c55c35ea3d0
Christopher Chin <ctchin13@gmail.com>
2016-05-24 22:35:47 -0500

    temperature converter

---
+++ README.md
@@ -0,0 +1,7 @@
+# Plain Web Utilities
+
+Just some utility programs written in javascript on a white backdrop.
+
+Currently doesn't use any static site generators. Compile the main sass file into base.css.
+
+`sass main.scss base.css`
---
+++ convert_temp.js
@@ -0,0 +1,30 @@
+function convertTemp() {
+    // DOM Elements of interest
+    var inputElem = document.getElementById("text-input");
+    var outputF = document.getElementById("fahrenheit");
+    var outputC = document.getElementById("celsius");
+
+    // Clear current output
+    outputF.innerHTML = "";
+    outputC.innerHTML = "";
+
+    // Get input and convert.
+    var text = inputElem.value;
+    var tempValue = parseFloat(text);
+    if (tempValue) {
+        inputElem.value = tempValue;
+
+        // Given value could be both F or C, convert to both.
+        var fahrenheit = tempValue * 9 / 5 + 32;
+        var celsius = 5 / 9 * (tempValue - 32);
+        outputF.innerHTML = fahrenheit.toPrecision(4) + "°F";
+        outputC.innerHTML = celsius.toPrecision(4) + "°C";
+    }
+
+    return false; // Prevents page reload on form submission
+}
+
+// Focus input on load
+window.onload = function() {
+    document.getElementById("text-input").focus();
+}
