1d0068b6c54cf7d05cf161223592be8f79e63b25
Christopher Chin <ctchin13@gmail.com>
2016-08-20 12:00:15 -0400

    soundboard.tmp

---
+++ soundboard/soundboard.js
@@ -0,0 +1,35 @@
+var audioURL;
+var inputElem = document.getElementById("file-input");
+var buttonElem = document.getElementById("submit");
+var label = document.getElementById('upload');
+var audio = document.getElementById("audio");
+
+function onInput() {
+    //audio.children[0].src = audioURL; // audio child is source
+    audio.pause();
+    audio.currentTime = 0;
+    audio.src = audioURL;
+    audio.play();
+
+    return false; // Prevents page reload on form submission
+}
+
+function onUpload() {
+    // Save file location
+    audioURL = URL.createObjectURL(inputElem.files[0]);
+
+    // Remove upload button
+    label.style.display = 'none';
+
+    // Show play button
+    buttonElem.style.display = 'block';
+
+    return false;
+}
+
+audio.addEventListener("error", function() {
+    // Show upload again
+    label.style.display = 'block';
+    buttonElem.style.display = 'none';
+});
+
