1e1c414cbd84373cf462b95c85adf9632a7b245c
Christopher Chin <ctchin13@gmail.com>
2016-08-31 20:05:33 -0400

    Pronounce. Used dictionary API instead.

--- pronounce/pronounce.js
+++ pronounce/pronounce.js
@@ -16,17 +16,35 @@ function onInput() {
     // To listen for a File Not Found error we have to bind to the source tag
     var audio = new Audio();
     var source = document.createElement("source");
-    source.src = "http://www.onelook.com/pronounce/macmillan/US/" + text +
-        "-American-English-pronunciation.mp3";
-    audio.appendChild(source);
-    audio.volume = 0.5;
 
-    source.addEventListener("error", function() {
-        inputElem.value = "";
-        errorMessage();
-    });
+    // Request the dictionary API
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=" + text + "&audio=pronunciation");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            var results = response.results;
 
-    audio.play();
+            for (var i = 0; i < results.length; i++) {
+                // Only exact matches
+                if (results[i].headword != text)
+                    continue;
+
+                source.src = "http://api.pearson.com" + results[i].pronunciations[0].audio[0].url;
+                console.log(source.src);
+                audio.appendChild(source);
+                audio.volume = 0.5;
+
+                source.addEventListener("error", function() {
+                    inputElem.value = "";
+                    errorMessage();
+                });
+
+                audio.play();
+            }
+        }
+    }
+    xhr.send();
 
 
     return false; // Prevents page reload on form submission
