ec662c22f0cdd69c6dde22b0347a4d5db1cdd51d
Christopher Chin <ctchin13@gmail.com>
2017-02-10 15:22:22 +0000

    notepad local storage.

--- notepad/notepad.js
+++ notepad/notepad.js
@@ -1,14 +1,30 @@
 // DOM Elements of interest
 var inputElem = document.getElementById("text-input");
 
-// Focus input on load
 window.onload = function() {
     inputElem.onkeypress = function() {
+        // Remove page title
         document.getElementById("title").style.display = "none";
 
         // Only call this function once
         inputElem.onkeypress = "";
     }
 
+    if (typeof(Storage) !== "undefined") {
+        inputElem.onchange = function() {
+            localStorage.setItem("notepad", inputElem.value);
+        };
+    }
+
     inputElem.focus();
+
+    // Try to retrieve localStorage
+    if (typeof(Storage) !== "undefined") {
+        inputElem.value = localStorage.getItem("notepad");
+
+        // Call "first input" handler if we have stored data.
+        if (inputElem.value !== "")
+            inputElem.onkeypress();
+    }
 }
+
